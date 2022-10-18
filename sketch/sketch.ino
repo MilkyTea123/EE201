52d3efb9d40f5d714a074c379c6c9c03fdd7695e
Michael Tsien <michael@Michaels-MacBook-Pro-8.local>
2022-10-18 14:12:24 -0700

    ino file

---
+++ sketch/sketch.ino
@@ -0,0 +1,23 @@
+https://wokwi.com/projects/345892300688196178
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(A0, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+}
+
+void loop() {
+  int value = analogRead(A0);
+
+  double per = ((150 * value) / 145) + 50;
+  Serial.println(per);
+
+  if (per >= 0) {
+    digitalWrite(2, HIGH);
+    delay(per);
+    digitalWrite(2, LOW);
+    delay(per);
+  }
+}
